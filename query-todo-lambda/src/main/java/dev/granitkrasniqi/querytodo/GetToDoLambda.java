dab265e9b943ce5c7ae24f6af0a0f28b436b2074
Granit Krasniqi <krasniqigranit59@gmail.com>
2021-01-10 13:27:43 +0100

    ADDED fetch todo by id functionality

--- query-todo-lambda/src/main/java/dev/granitkrasniqi/querytodo/GetToDoLambda.java
+++ query-todo-lambda/src/main/java/dev/granitkrasniqi/querytodo/GetToDoLambda.java
@@ -1,7 +1,9 @@
 package dev.granitkrasniqi.querytodo;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,15 +12,21 @@ import java.io.IOException;
 
 public class GetToDoLambda {
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+    private final DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
     private final String tableName = System.getenv("TODO_TABLE");
 
     public APIGatewayProxyResponseEvent handler(APIGatewayProxyRequestEvent request) throws IOException {
         final String toDoId = request.getPathParameters().get("id");
-        System.out.println("toDoId " + toDoId);
+        Table toDoTable = dynamoDB.getTable(tableName);
+        Item item = toDoTable.getItem("id", toDoId);
+
+        if (item == null) {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(404);
+        }
 
         return new APIGatewayProxyResponseEvent()
                 .withStatusCode(200)
-                .withBody("");
+                .withBody(item.toJSON());
     }
 }
